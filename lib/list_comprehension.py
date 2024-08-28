652fd57036a70cdb492eb7cd41796d07c70501f9
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-08-28 12:14:09 +0300

    updates lc and ge

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if not num_list:
+        return []
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return []
+    return [f"{sentence}!" for sentence in sentence_list]
